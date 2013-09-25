2ceb0dd3d93b3684291c4e32f6c333588d08cc1e
Mathijs Saey <mathsaey@vub.ac.be>
2013-09-25 22:55:42 +0200

    documentation update

---
+++ search/all_71.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['quicksort_20in_20if1',['Quicksort in IF1',['../_q_s_i_f1.html',1,'']]],
+  ['quicksort_20in_20sisal',['Quicksort in Sisal',['../_q_s_sis.html',1,'']]]
+];
--- search/pages_69.js
+++ search/pages_69.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['if1',['IF1',['../md_docpages__i_f1.html',1,'']]]
+  ['if1_20quick_20reference',['IF1 Quick Reference',['../md_docpages__i_f1.html',1,'']]]
 ];
