f00981be163cbe4ffbee47e6bbba58b294cd6ff3
Mathijs Saey <mathsaey@vub.ac.be>
2013-09-27 15:40:21 +0200

    Setup of prototype

---
+++ prototypes/1.flow/main.rkt
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;
+;;;; DVM prototype ;;;;
+;;;; Mathijs Saey  ;;;;
+;;;; main.rkt      ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;
+
+; This is the top level file of the dvm prototype.
+; It's responsible for managing the various classes, 
+; it also contains the test code.
+
+#lang racket
+
+(include "node.rkt")
+(include "edge.rkt")
+
