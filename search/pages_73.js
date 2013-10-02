fb43b3b880b36c7c3d4a1610e0a2090d7d275e95
Mathijs Saey <mathsaey@vub.ac.be>
2013-10-03 00:30:40 +0200

    Documentation update

--- search/pages_73.js
+++ search/pages_73.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['software',['Software',['../md_docpages_software.html',1,'']]]
+  ['software',['Software',['../md_docpages_software.html',1,'']]],
+  ['sorting_20algorithm_20in_20if1',['Sorting algorithm in IF1',['../_sort_i_f1.html',1,'']]],
+  ['sorting_20algorithm_20in_20sisal',['Sorting algorithm in Sisal',['../_sort_sis.html',1,'']]]
 ];
