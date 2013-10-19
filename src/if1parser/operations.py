22c9d9dcfc48d2c9d77cf98e4ec6f859209594d8
Mathijs Saey <mathsaey@vub.ac.be>
2013-10-19 17:09:51 +0200

    Added part of the default operations

--- src/if1parser/operations.py
+++ src/if1parser/operations.py
@@ -8,6 +8,9 @@ IF1 nodes.
 
 """
 
+from functools import partial
+from math import floor
+
 # ---------------- #
 # Public functions #
 # ---------------- #
@@ -19,39 +22,140 @@ def getFunction(label): pass
 # Function Definitions #
 # -------------------- #
 
-def NoOp(*args): pass
+def NoOp(*args): 					pass
+def ALimL(arr): 					return 0
+def Not(x):							return not x
+def Mod(l,r):						return l % r
+def Minus(l,r):						return l - r
+def Exp(l, r): 						return l ** r
+def Max(l,r): 						return max(l,r)
+def Min(l,r):						return min(l,r)
+def ASize(arr): 					return len(arr)
+def Less(l,r): 						return l < r
+def Div(l, r): 						return l / r
+def Equal(l, r): 					return l == r
+def NotEqual(l, r): 				return l != r
+def LessEqual(l,r):					return l <= r
+def Neg(arith):						return - arith
+def ARemL(arr): 					return arr[1:]
+def ALimH(arr): 					return len(arr)
+def Bool(int): 						return bool(int)
+def Abs(arith): 					return abs(arith)
+def Floor(int): 					return floor(int)
+def Double(int): 					return int + 0.0
+def AIsEmpty(arr): 					return arr == []
+def RangeGenerate(l, h): 			return range(l,h)
+def Single(val): 					return float(val)
+def Char(int): 						return str(unichr(int))
+def AAddL(arr, el): 				return [el] + arr
+def AElement(arr, idx): 			return arr[idx]
+def ARemH(arr): 					return arr[:len(arr) - 1]
+def AAddH(arr, el): 				return arr[:] + [el]
+def ABuild(bound, *elements): 		return list(elements)
+def BindArguments(func, *args): 	return partial(func, args)
 
-def AAddH(arr, el): 
-	return arr += [el]
+def RedLeft(func, acc, mult, filt): 
+	print "TODO: see if foldl is still needed with compound nodes"
+def RedRight(func, acc, mult, filt): 
+	print "TODO: see if foldl is still needed with compound nodes"
+def RedTree(func, acc, mult, filt): 
+	print "TODO: see if foldl is still needed with compound nodes"
+def Reduce(func, acc, mult, filt): 
+	print "TODO: see if foldl is still needed with compound nodes"
 
-def AAddL(arr, el):
-	return [el] += arr
+def Call(func, *args): 				
+	print "TODO: add higher order call..."
+def ASetL(arr, idx): 				
+	print "ASetL not supported in IF1 subset..."
+def AScatter(arr): 					
+	print "TODO: see if ASCatter is still needed with compound nodes"
 
-def ABuild(bound, *elements):
-	return list(elements)
+def IsError(l,r): 
+	print "TODO: create internal error type"
+	return (l == r) or (l == "error" and isinstance(r, str))
+
 
 def ACatenate(arr, *arrs):
-	res = arr
+	res = arr[:]
 	for el in arrs:
 		res += el
 	return res
 
-def AElement(arr, idx):
-	return arr[idx]
+def AFill(l, h, *el):
+	if h > l:
+		return list(el) * (h - l)
+	else: 
+		return []
+
+def AGather(l, arr, filter = None):
+	res = []
+	filt = filter
+	if filt is None:
+		filt = [True] * len(arr)
+
+	for idx in xrange(0,len(arr)):
+		el = arr[idx]
+		b = filt[idx]
+		if b:
+			res += el
+	return res
+
+def AReplace(arr, idx, *args):
+	end = idx + len(args)
+	res = arr[:]
+	res[idx:end] = args
+	return res
+
+def FirstValue(mult, filter = None):
+	filt = filter
+	if filt is None:
+		filt = [True] * len(mult)
+
+	for idx in xrange(0,len(mult)):
+		if filt[idx]:
+			return mult[idx]
+
+def FinalValue(mult, filter = None):
+	filt = filter
+	mult.reverse()
+	if filt is None:
+		filt = [True] * len(mult)
+
+	for idx in xrange(0,len(mult)):
+		if filt[idx]:
+			return mult[idx]
+
+def Int(x):
+	if isinstance(x, float):
+		x += 0.5
+	return int(x)
+
+def Plus(l,r):
+	if isinstance(l, bool):
+		return l or r
+	else:
+		return l + r
+
+def Times(l,r):
+	if isinstance(l, bool):
+		return l and r
+	else:
+		return l * r
 
 # ---------------- #
 # Function Mapping #
 # ---------------- #
 
-from functools import partial
-
-def unkownFunction(name, *args):
+def unkownFunctionError(name, *args):
 	print "Undefined IF1 function with name:", name, "encountered."
+
+def createPartial(name):
+	return partial(unkownFunctionError, name)
 
 _functions = {
 	100 : AAddH,
 	101 : AAddL,
-	102 : partial(unkownFunction, "AAdjust")
+	102 : createPartial("AAdjust"),
 	103 : ABuild,
 	104 : ACatenate,
 	105 : AElement,
@@ -92,54 +196,54 @@ _functions = {
 	140 : NotEqual,
 	141 : Plus,
 	142 : RangeGenerate,
-	143 : RBuild,
-	144 : RElements,
-	145 : RReplace,
-	146 : RedLeft,
-	147 : RedRight,
-	148 : RedTree,
-	149 : Reduce,
-	150 : RestValues,
+	143 : createPartial("RBuild"),
+	144 : createPartial("RElements"),
+	145 : createPartial("RReplace"),
+	146 : createPartial("RedLeft"),
+	147 : createPartial("RedRight"),
+	148 : createPartial("RedTree"),
+	149 : createPartial("Reduce"),
+	150 : createPartial("RestValues"),
 	151 : Single,
 	152 : Times,
-	153 : Trunc,
-	154 : PrefixSize,
-	155 : Error,
-	156 : ReplaceMulti,
-	157 : Convert,
-	158 : CallForeign,
-	159 : AElementN,
-	160 : AElementP,
-	161 : AElementM,
-	170 : AAddLAT,
-	171 : AAddHAT,
-	172 : ABufPartition,
-	173 : ABuildAT,
-	174 : ABufScatter,
-	175 : ACatenateAT,
-	176 : AElementAT,
-	177 : AExtractAT,
-	178 : AFillAT,
-	179 : AGatherAT,
-	180 : ARemHAT,
-	181 : ARemLAT,
-	182 : AReplaceAT,
-	183 : ArrayToBuf,
-	184 : ASetLAT,
-	185 : DefArrayBuf,
-	186 : DefRecordBuf,
-	187 : FinalValueAT,
-	188 : MemAlloc,
-	189 : BufElements,
-	190 : RBuildAT,
-	191 : RecordToBuf,
-	192 : RElementsAT,
-	193 : ReduceAT,
-	19  : ShiftBuffer,
-	195 : ScatterBufPartitions,
-	196 : RedLeftAT,
-	197 : RedRightAT,
-	198 : RedTreeAT
+	153 : createPartial("Trunc"),
+	154 : createPartial("PrefixSize"),
+	155 : createPartial("Error"),
+	156 : createPartial("ReplaceMulti"),
+	157 : createPartial("Convert"),
+	158 : createPartial("CallForeign"),
+	159 : createPartial("AElementN"),
+	160 : createPartial("AElementP"),
+	161 : createPartial("AElementM"),
+	170 : createPartial("AAddLAT"),
+	171 : createPartial("AAddHAT"),
+	172 : createPartial("ABufPartition"),
+	173 : createPartial("ABuildAT"),
+	174 : createPartial("ABufScatter"),
+	175 : createPartial("ACatenateAT"),
+	176 : createPartial("AElementAT"),
+	177 : createPartial("AExtractAT"),
+	178 : createPartial("AFillAT"),
+	179 : createPartial("AGatherAT"),
+	180 : createPartial("ARemHAT"),
+	181 : createPartial("ARemLAT"),
+	182 : createPartial("AReplaceAT"),
+	183 : createPartial("ArrayToBuf"),
+	184 : createPartial("ASetLAT"),
+	185 : createPartial("DefArrayBuf"),
+	186 : createPartial("DefRecordBuf"),
+	187 : createPartial("FinalValueAT"),
+	188 : createPartial("MemAlloc"),
+	189 : createPartial("BufElements"),
+	190 : createPartial("RBuildAT"),
+	191 : createPartial("RecordToBuf"),
+	192 : createPartial("RElementsAT"),
+	193 : createPartial("ReduceAT"),
+	19  : createPartial("ShiftBuffer"),
+	195 : createPartial("ScatterBufPartitions"),
+	196 : createPartial("RedLeftAT"),
+	197 : createPartial("RedRightAT"),
+	198 : createPartial("RedTreeAT")
 }
 
 def getFunction(label):
