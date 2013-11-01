f208cb3db48a52668e0c63c8bfc5b286db5b0267
Mathijs <mathijssaey@gmail.com>
2013-11-01 15:46:46 +0100

    Cleaned up literal

--- src/core/literal.py
+++ src/core/literal.py
@@ -32,9 +32,9 @@ import runtime
 
 class Literal(object):	
 	def __init__(self, destination, value):
-		self.destination = destination
 		self.value = value
+		self.destination = destination
 		runtime.main.addLiteral(self)
 
 	def activate(self):
-		self.destination.acceptInput(self.value, True)
+		self.destination.receiveInput(self.value, True)
