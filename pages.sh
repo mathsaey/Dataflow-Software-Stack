7cd512ad9001702a48fccd892c14de151172d6cb
mathsaey <mathsaey@vub.ac.be>
2014-01-18 02:13:02 +0100

    Documentation update

--- pages.sh
+++ pages.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-
 # Quick script to add documentation to gh pages
 
+pushd $PWD
+
+# Find the current dir location
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Add al fils to git, commit and push
+# Useful commits are not needed since
+# this is a documentation branch
 git add *
 git commit -a -m "Documentation update"
 git push origin gh-pages
+
+popd
