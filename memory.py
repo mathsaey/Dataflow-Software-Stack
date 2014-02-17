3089c071e8baea5ffaeae89cf7d1e09232fd4307
mathsaey <mathsaey@vub.ac.be>
2014-02-17 21:47:13 +0100

    Instruction memory changes
    - Fixed some bugs
    - Changed naming of slot reservations to better reflect their purpose
    - Added reset function
    - Added a function that checks if a key needs a matcher

--- memory.py
+++ memory.py
@@ -42,29 +42,29 @@ class InstructionMemory(object):
 	def __init__(self):
 		super(InstructionMemory, self).__init__()
 		self.memory = {}
-		self.strKey = -1
-		self.trlKey = 0
+		self.matcherKey = 0
+		self.standardKey = -1
 
 	## Get a single instruction from memory
 	def getInstruction(self, key):
 		return self.memory[key]
 
 	## 
-	# Reserve a slot for a STR (single token receiver)
-	# token.
+	# Reserve a slot for an instruction that does
+	# not require any context matching.
 	##
-	def reserveSTRSlot(self):
-		key = self.strKey
-		self.strKey -= 1
+	def reserveStandardSlot(self):
+		key = self.standardKey
+		self.standardKey -= 1
 		return key
 
 	##
-	# Reserve a slot for a TLR (token list receiver)
-	# token.
+	# Reserve a slot for an instruction that
+	# requires context matching.
 	##
-	def reserveTLRSlot(self):
-		key = self.trlKey
-		self.trlKey += 1
+	def reserveMatcherSlot(self):
+		key = self.matcherKey
+		self.matcherKey += 1
 		return key
 
 	##
@@ -73,12 +73,10 @@ class InstructionMemory(object):
 	def add(self, inst):
 		key = None
 
-		if inst.isSTR():
-			key = self.reserveSTRSlot()
-		elif inst.isTLR():
-			key = self.reserveTLRSlot()
+		if inst.needsMatcher():
+			key = self.reserveMatcherSlot()
 		else:
-			print "INS", "Memory received invalid instruction", inst
+			key = self.reserveStandardSlot()
 
 		self.memory.update({key : inst})
 		inst.setKey(key)
@@ -96,8 +94,24 @@ __MEMORY__ = InstructionMemory()
 ## Get a reference to the instruction memory
 def memory(): return __MEMORY__
 
+## Delete all the contents of the instruction memory.
+def reset(): 
+	global __MEMORY__ 
+	__MEMORY__= InstructionMemory()
+
 ## Get an instruction from the main memory
 def get(key): return memory().get(key)
 
 ## Add an instruction to the main memory
-def add(inst): return memory.add(inst)
+def add(inst): return memory().add(inst)
+
+## 
+# See if an instruction needs to pass the matcher
+# Instructions that require a context manager are stored
+# in a separate part of the memory, so checking the key suffices.
+#
+# \param key
+#		The key of the instruction 
+# \return 
+#		True if the instruction needs to be matched.
+def needsMatcher(key): return key >= 0
