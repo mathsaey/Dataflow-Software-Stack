03e7a5e56923cf9bc70e3a34986eca7486eff7d6
mathsaey <mathsaey@vub.ac.be>
2014-02-23 15:08:59 +0100

    Documentation update

--- pages.sh
+++ pages.sh
@@ -10,7 +10,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 # Add al fils to git, commit and push
 # Useful commits are not needed since
 # this is a documentation branch
-git add *
+git add .
 git commit -a -m "Documentation update"
 git push origin gh-pages
 
