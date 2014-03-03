5e469bee72dd3e28710e8064ec8b153c922ea07d
mathsaey <mathsaey@vub.ac.be>
2014-03-03 23:22:02 +0100

    Documentation update

--- search/variables_8.js
+++ search/variables_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['hash',['hash',['../classcore_1_1context_1_1_context.html#a397854106f13a8c74cd34b86b67257f7',1,'core::context::Context']]]
+  ['hash',['hash',['../classcore_1_1context_1_1_context.html#a397854106f13a8c74cd34b86b67257f7',1,'core::context::Context']]],
+  ['haslit',['hasLit',['../classcore_1_1instruction_1_1_operation_instruction.html#a42864c031d933b97c4368c9c73897010',1,'core::instruction::OperationInstruction']]]
 ];
