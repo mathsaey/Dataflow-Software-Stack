5f4df6f0ed56917943bd7eb4f3f797a90b96e5f1
mathsaey <mathsaey@vub.ac.be>
2014-03-10 03:12:00 +0100

    Documentation update

--- search/variables_6.js
+++ search/variables_6.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['format_5fstr',['FORMAT_STR',['../namespacelog.html#a980b268fc3ec194b35ea6fa22e3e082e',1,'log']]],
   ['frontend',['frontEnd',['../namespacefront_end.html#a22bc3d2d929d9c4953bcb0d40677cd59',1,'frontEnd']]],
+  ['frontends',['frontEnds',['../namespacedisc.html#a01640a56051d07e0d0a8093edb175fa0',1,'disc']]],
   ['function',['function',['../class_i_g_r_1_1node_1_1_call_node.html#ab593140999625b13121700d459aaf89b',1,'IGR::node::CallNode']]],
   ['functions',['functions',['../namespaceread.html#a08937222a213e2af5ca289687b490b71',1,'read']]]
 ];
